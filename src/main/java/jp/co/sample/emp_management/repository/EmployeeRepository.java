ebee2bafcc886523c292413a5c32362fbcfb9c4a
Tomoki <84486284+tomoki-rks@users.noreply.github.com>
2021-06-08 10:10:23 +0900

    Repositoryにあいまい検索を実装

--- src/main/java/jp/co/sample/emp_management/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/emp_management/repository/EmployeeRepository.java
@@ -74,6 +74,16 @@ public class EmployeeRepository {
 		return development;
 	}
 
+	public List<Employee> findByName(String name) {
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM employees WHERE name LIKE :name";
+
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+
+		List<Employee> employeeList = template.query(sql, param, EMPLOYEE_ROW_MAPPER);
+
+		return employeeList;
+	}
+
 	/**
 	 * 従業員情報を変更します.
 	 */
