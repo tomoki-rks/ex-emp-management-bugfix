1b172ef4ea359a2c106e3c0ee4644cbf207c8798
tomoki-rks <84486284+tomoki-rks@users.noreply.github.com>
2021-06-11 18:00:49 +0900

    feature/5-4.ログイン認証フィルター (#21)
    * ページネーションのためにあいまい検索と全件表示の処理を統合
    
    * 認証機能の設定を追加
    
    * ログイン認証フィルターの実装に伴い、自分で実装したログイン機能を削除
    
    * ログイン認証フィルターをServiceに追加

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -107,25 +106,6 @@ public class AdministratorController {
 	public String toLogin() {
 		return "administrator/login";
 	}
-
-	/**
-	 * ログインします.
-	 * 
-	 * @param form   管理者情報用フォーム
-	 * @param result エラー情報格納用オブッジェクト
-	 * @return ログイン後の従業員一覧画面
-	 */
-	@RequestMapping("/login")
-	public String login(LoginForm form, BindingResult result, Model model) {
-		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
-		if (administrator == null) {
-			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
-			return toLogin();
-		}
-		session.setAttribute("administratorName", administrator.getName());
-		return "forward:/employee/showList";
-	}
-
 	/////////////////////////////////////////////////////
 	// ユースケース：ログアウトをする
 	/////////////////////////////////////////////////////
